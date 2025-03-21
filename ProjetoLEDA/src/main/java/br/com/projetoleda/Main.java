39384543e29d11ea34996116ff5baa3f3f4cd684
Arthur Barbosa <arthurdonatobarbosa110@gmail.com>
2025-03-21 17:47:26 -0300

    Gerando o primeiro arquivo com as datas formatadas

---
+++ ProjetoLEDA/src/main/java/br/com/projetoleda/Main.java
@@ -0,0 +1,13 @@
+package br.com.projetoleda;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+
+        GameCSV.escrevendoArquivoComDatasFormatadas();
+
+    
+    }
+}
